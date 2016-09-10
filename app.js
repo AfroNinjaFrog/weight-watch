edcbc6bc1e1ad9b0f45c834d3131466e2d1add1e
Sagy Grimberg <sagythe@gmail.com>
2016-09-10 10:41:45 +0300

    good base

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+var jsonfile = require('jsonfile');
 app.use('/assets', express.static('public'));
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('views',__dirname + '/public');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.get('/api', function (req, res) {
+  res.send([{data: "data"},{data:"data"}]);
 });
-
+app.get('/', function (req, res) {
+  res.render('views/index.html')
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+app.post('/Weights', function(req, res) {  
+  var file = 'data.json';
+  jsonfile.writeFile(file, req.body, function (err) {
+    console.error(err);
+  });
 });
